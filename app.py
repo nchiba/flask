1db03c4980acb86c2f13a2cb7c8d644f41bb9a82
Naoki Chiba <nchiba@gmail.com>
2021-11-23 22:00:14 +0900

    change app.run()

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, Chiba!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
